4890927c100e616281571919732abf11e1b9c0e6
asubelza <subelzaalfonso@gmail.com>
2026-04-02 13:11:46 -0300

    Fix: add React import for TypeScript

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { useState, createContext, useContext } from 'react'
+import React, { useState, createContext, useContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material'
 import App from './App.tsx'
